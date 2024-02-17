0bcaa7bdc1a18ce2fcfcddd9aed6ed4a6570f1a7
ranawasifwaheed <ranawasifwaheed@gmail.com>
2024-02-18 02:20:18 +0500

    new updated code

--- multiSession/test.js
+++ multiSession/test.js
@@ -1,216 +1,51 @@
-const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const { PrismaClient } = require('./generated/client');
-const cors = require('cors');
+const qrcode = require('qrcode-terminal');
+const express = require('express');
+const bodyParser = require('body-parser');
 const qr = require('qr-image');
-const { exit } = require('process');
-const prisma = new PrismaClient();
+const cors = require('cors');
 const app = express();
 const port = 781;
-
-app.use(express.json());
 app.use(cors({
     origin: '', 
     methods: ['GET', 'POST']
 }));
+app.use(bodyParser.json());
 
 
-async function createAndInitializeClient(clientId, phone_number, res) {
+app.post('/initialize-client', async (req, res) => {
+    const { clientId } = req.body;
+
     try {
-        if (!clientId || !phone_number) {
-            throw new Error('Client ID and phone number are required.');
-        }
-        console.log('inlitizng')
-
-        let qrCodeData;
         const client = new Client({
-            authStrategy: new LocalAuth({ clientId: clientId }),
+            authStrategy: new LocalAuth({ clientId }),
             puppeteer: {
-                headless: true,
-               // args: ["--no-sandbox",'--proxy-server=147.185.238.169:50002']
-                args: ["--no-sandbox",'--proxy-server=46.166.137.38:31499']
+              headless: true,
+            //   args: ["--no-sandbox", '--proxy-server=46.166.137.38:31499']
             }
-        });
-
-        console.log('initiliazed')
-        // Define the QR event listener
-        const onQRReceived = async (qrCode) => {
-            console.log("running.....................................")
-            return 'thank yu ';
-
+          });
+        client.on('qr', (qrCode) => {
             console.log('QR RECEIVED', qrCode);
-
-            
-
-            const timestamp = Date.now();
-            qrCodeData = qrCode;
-
             const qrImage = qr.image(qrCode, { type: 'png' });
             qrImage.pipe(res, { end: true });
+        });
 
-            console.log(`Client ${clientId} generated`);
-            await prisma.session.upsert({
-                where: { clientId },
-                update: { phone_number, qrCodeData, createdAt: timestamp },
-                create: { clientId, phone_number, qrCodeData, createdAt: timestamp },
-            });
+        client.on('ready', () => {
+            console.log(`Client is ready! ${clientId}`);
+        });
 
-            client.removeListener('qr', onQRReceived);
-        };
-
-        client.on('qr', onQRReceived);
-
-        client.on('ready', async () => {
-            console.log('Client is ready!');
-            await prisma.session.upsert({
-                where: { clientId },
-                update: { status: 'ready', qrCodeData },
-                create: { clientId, status: 'ready', phone_number, qrCodeData },
-            });
-            sendStatusResponse(clientId, phone_number, res);
+        // client.on('message', (message) => {
+        //     console.log(message.body);
+        // });
+ 
+        client.on('disconnected', (reason) => {
+            console.log('Client was logged out', reason);
         });
 
         client.initialize();
-        return client;
     } catch (error) {
-        console.error('Error creating and initializing client:', error.message);
-        res.status(400).json({ error: error.message });
-    }
-}
-
-
-async function sendStatusResponse(clientId, phone_number, res) {
-    try {
-        const session = await prisma.session.findUnique({
-            where: { 
-                clientId,
-                phone_number,
-            },
-        });
-
-        if (!session) {
-            res.status(404).json({ error: 'Client not found.' });
-        } else {
-            res.status(200).json({
-                clientId: session.clientId,
-                status: session.status || 'unknown',
-                phone_number: phone_number || session.phone_number,
-            });
-        }
-    } catch (error) {
-        console.error('Error fetching session status:', error.message);
-        res.status(500).json({ error: 'Internal server error.' });
-    }
-}
-
-async function sendMessage(client, to, message) {
-    try {
-        await client.sendMessage(to, message);
-        console.log('Message sent successfully.');
-    } catch (error) {
-        console.error('Error sending message:', error.message);
-        throw error;
-    }
-}
-
-app.get('/generateClient', async (req, res) => {
-    const clientId = req.query.clientId;
-  
-    const previousSession = await prisma.session.findUnique({
-      where: { clientId: clientId },
-    });
-  
-    const client = new Client({
-      authStrategy: new LocalAuth({ clientId: clientId }),
-      puppeteer: {
-        headless: true,
-        args: ["--no-sandbox", '--proxy-server=46.166.137.38:31499']
-      }
-    });
-  
-    if (!previousSession) {
-      // If no previous session, generate a new QR code and send it
-      const onQRReceived = async (qrCode) => {
-        console.log('QR RECEIVED', qrCode);
-  
-        const sessionData = {
-          clientId: clientId,
-          qrCodeData: qrCode,
-          phone_number: req.query.phone_number || null,
-          createdAt: Math.floor(Date.now() / 1000),
-        };
-  
-        await prisma.session.upsert({
-          where: { clientId: clientId },
-          update: { qrCodeData: qrCode },
-          create: sessionData,
-        });
-  
-        const qrImage = qr.image(qrCode, { type: 'png' });
-        
-        qrImage.pipe(res, { end: true });
-        
-  
-        console.log(`Client ${clientId} generated`);
-        client.removeListener('qr', onQRReceived);
-      };
-  
-      client.on('qr', onQRReceived);
-      client.initialize();
-    } else {
-      const qrImage = qr.image(previousSession.qrCodeData, { type: 'png' });
-      qrImage.pipe(res, { end: true });
-      console.log(`Sending QR code from the previous session for client ${clientId}`);
-      client.initialize();
-    }
-  });
-  
-
-app.get('/create-client', async (req, res) => {
-    const clientId = req.query.clientId;
-    const phone_number = req.query.phone_number;
-
-    try {
-        const client = await createAndInitializeClient(clientId, phone_number, res);
-    } catch (error) {
-        // Error handling is already done in the createAndInitializeClient function
-    }
-});
-
-app.get('/send-message', async (req, res) => {
-    const { clientId, phone_number, to, message } = req.query;
-
-    try {
-        const session = await prisma.session.findUnique({
-            where: {
-                clientId,
-                phone_number,
-            },
-        });
-
-        if (!session || session.status !== 'ready') {
-            res.status(404).json({ error: 'Client not found or not ready.' });
-            return;
-        }
-
-        const client = await createAndInitializeClient(clientId, session.phone_number, res);
-        await sendMessage(client, to, message);
-        res.status(200).json({ success: true, message: 'Message sent successfully.' });
-    } catch (error) {
-        console.error('Error sending message:', error.message);
-        res.status(500).json({ error: 'Internal server error.' });
-    } finally {
-        sendStatusResponse(clientId, phone_number, res);
-    }
-});
-
-app.get('/status', async (req, res) => {
-    const { clientId, phone_number } = req.query;
-
-    if (!clientId) {
-        res.status(400).json({ error: 'Client ID is required as a query parameter.' });
-    } else {
-        sendStatusResponse(clientId, phone_number, res);
+        console.error(`Error initializing client ${clientId}:`, error.message);
+        res.status(500).json({ error: `Error initializing client ${clientId}` });
     }
 });
 
