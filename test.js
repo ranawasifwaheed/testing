fa70e37d372338408e3968fbc8953e2fbbd5dc03
ranawasifwaheed <ranawasifwaheed@gmail.com>
2024-02-20 03:28:23 +0500

    add database code

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ const qrcode = require('qrcode-terminal');
 const express = require('express');
 const bodyParser = require('body-parser');
 const qr = require('qr-image');
+const { PrismaClient } = require("./generated/client");
+const prisma = new PrismaClient();
 const cors = require('cors');
 const app = express();
 const port = 781;
@@ -12,45 +14,79 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-const activeClients = {}; 
-
+const activeClients = {};
 app.get('/initialize-client', async (req, res) => {
-    const { clientId } = req.query;
+    const { clientId, phone_number } = req.query;
 
     try {
-        if (activeClients[clientId]) {
+        const existingClient = await prisma.qRCode.findFirst({
+            where: { clientId, status: 1 },
+        });
+
+        if (existingClient) {
             res.status(400).json({ error: `Client ${clientId} already initialized` });
             return;
         }
+
+        const existingQRCode = await prisma.qRCode.findFirst({
+            where: { clientId, status: 0 },
+        });
+
         const client = new Client({
             qrMaxRetries: 1,
             authStrategy: new LocalAuth({ clientId }),
             puppeteer: {
                 headless: true,
-                args: ["--no-sandbox", '--proxy-server=46.166.137.38:31499']
             }
         });
-        client.on('qr', (qrCode) => {
-            res.status(200).json({ qrcode: qrCode });
+        let updatedQRCode;
+
+        client.on('qr', async (qrCode) => {
+            
+            if (existingQRCode) {
+                updatedQRCode = await prisma.qRCode.update({
+                    where: { id: existingQRCode.id },
+                    data: { qrCode, phone_number }
+                });
+            } else {
+                updatedQRCode = await prisma.qRCode.create({
+                    data: { clientId, qrCode, phone_number, status: 0 }
+                });
+                
+            }
+
+            console.log(`QR RECEIVED for ${clientId}`, qrCode);
+            const qrImage = qr.image(qrCode, { type: 'png' });
+            qrImage.pipe(res, { end: true });
         });
 
-        client.on('ready', () => {
+        client.on('ready', async () => {
             console.log(`Client is ready for ${clientId}`);
+                await prisma.qRCode.update({
+                where: { id: updatedQRCode.id },
+                data: { status: 1}
+            });
             activeClients[clientId] = client;
         });
 
-        client.on('disconnected', (reason) => {
+        client.on('disconnected', async (reason) => {
             console.log(`Client for ${clientId} was logged out`, reason);
+            await prisma.qRCode.update({
+                where: { id: updatedQRCode.id },
+                data: { status: 0 }
+            });
             delete activeClients[clientId];
         });
 
         client.initialize();
-        
+
     } catch (error) {
-        console.error(`Error initializing client ${clientId}:`, error.message);
-        res.status(500).json({ error: `Error initializing client ${clientId}` });
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
 });
+
+
 app.get('/status', (req, res, next) => {
     const requestedClientId = req.query.clientId;
 
@@ -67,23 +103,35 @@ app.get('/status', (req, res, next) => {
     }
 });
 
-app.get('/message', (req, res, next) => {
+app.get('/message', async (req, res, next) => {
     const requestedClientId = req.query.clientId;
     const number = req.query.to;
     const text = req.query.message;
 
-    if (!requestedClientId && number && text ) {
-        res.status(400).json({ error: 'clientId, number and text are required in the query parameters' });
+    if (!requestedClientId || !number || !text) {
+        res.status(400).json({ error: 'clientId, number, and text are required in the query parameters' });
         return;
     }
 
-    const client = activeClients[requestedClientId];    
-    if (client) {
+    const client = activeClients[requestedClientId];
 
-       const chatId = number.substring(1) + "@c.us";
-       client.sendMessage(chatId, text);
-            res.status(200).json({ message: `Client ${requestedClientId} send message successfully` });
-        
+    if (client) {
+        const chatId = number.substring(1) + "@c.us";
+        client.sendMessage(chatId, text);
+
+        try {
+            await prisma.messageLog.create({
+                data: {
+                    clientId: requestedClientId,
+                    phoneNumber: number,
+                    message: text,
+                },
+            });
+            res.status(200).json({ message: `Client ${requestedClientId} sent message successfully` });
+        } catch (error) {
+            console.error("Error saving message log:", error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     } else {
         res.status(404).json({ error: `Client ${requestedClientId} not found or not ready` });
     }
