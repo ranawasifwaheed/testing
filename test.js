24723e318a5552f7c9403332f5e8342c17c4b979
Rana Muhammad Wasif Waheed <ranawasifwaheed@gmail.com>
2024-06-04 16:33:33 +0500

    Update test.js

--- test.js
+++ test.js
@@ -43,72 +43,83 @@ app.get('/initialize-client', async (req, res) => {
             restartOnAuthFail: true,
             webVersion: '2.2409.2',
             webVersionCache: {
-            type: 'remote',
-            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
-        },
+                type: 'remote',
+                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
+            },
             puppeteer: {
                 headless: true,
-               // args: ["--no-sandbox",'--proxy-server=147.185.238.169:50002']
-                args: ["--no-sandbox",'--proxy-server=46.166.137.38:31499']
+                args: ["--no-sandbox", '--proxy-server=46.166.137.38:31499']
             }
         });
 
-        client.initialize([clientId]);
+        client.initialize();
 
         let updatedQRCode;
 
         client.on('qr', async (qrCode) => {
-            if (existingQRCode) {
-                updatedQRCode = await prisma.qRCode.update({
-                    where: { id: existingQRCode.id },
-                    data: { qrCode }
-                });
-            } else {
-                updatedQRCode = await prisma.qRCode.create({
-                    data: { clientId, qrCode, status: 0 }
-                });
-            }
+            try {
+                if (existingQRCode) {
+                    updatedQRCode = await prisma.qRCode.update({
+                        where: { id: existingQRCode.id },
+                        data: { qrCode }
+                    });
+                } else {
+                    updatedQRCode = await prisma.qRCode.create({
+                        data: { clientId, qrCode, status: 0 }
+                    });
+                }
 
-            console.log(`QR RECEIVED for ${clientId}`, qrCode);
-            const qrImage = qr.image(qrCode, { type: 'png' });
-            qrImage.pipe(res, { end: true });
+                console.log(`QR RECEIVED for ${clientId}`, qrCode);
+                const qrImage = qr.image(qrCode, { type: 'png' });
+                qrImage.pipe(res, { end: true });
+            } catch (error) {
+                console.error("Error updating QR code in the database:", error);
+                res.status(500).json({ error: "Internal server error" });
+            }
         });
 
         client.on('ready', async () => {
-            const user = client.info.me.user;
-            console.log('User:', user);
-            console.log(`Client is ready for ${clientId}`);
-            await prisma.qRCode.update({
-                where: { id: updatedQRCode.id },
-                data: { status: 1, phone_number: user }
-            });
-            activeClients[clientId] = client;
+            try {
+                const user = client.info.me.user;
+                console.log('User:', user);
+                console.log(`Client is ready for ${clientId}`);
+                await prisma.qRCode.update({                    where: { id: updatedQRCode.id },
+                    data: { status: 1, phone_number: user }
+                });
+                activeClients[clientId] = client;
+            } catch (error) {
+                console.error("Error updating client status in the database:", error);
+            }
         });
 
         client.on('authenticated', () => {
-            console.error(clientId + ' authenticated');
+            console.log(`${clientId} authenticated`);
         });
 
-        client.on('auth_failure', () => {
-            console.log('auth_failure', );
+        client.on('auth_failure', (msg) => {
+            console.error('Authentication failed:', msg);
         });
 
-        client.on('disconnected', async () => {
-            await prisma.qRCode.update({
-                where: { id: updatedQRCode.id },
-                data: { status: 0 }
-            });
-            delete activeClients[clientId];
+        client.on('disconnected', async (reason) => {
+            console.log(`Client for ${clientId} disconnected:`, reason);
+            try {
+                await prisma.qRCode.update({
+                    where: { id: updatedQRCode.id },
+                    data: { status: 0 }
+                });
+                delete activeClients[clientId];
+            } catch (error) {
+                console.error("Error updating client status in the database after disconnection:", error);
+            }
         });
 
     } catch (error) {
-        console.error("Error:", error);
+        console.error("Error initializing client:", error);
         res.status(500).json({ error: "Internal server error" });
     }
-})
+});
 
-
-app.get('/status', (req, res, next) => {
+app.get('/status', (req, res) => {
     const requestedClientId = req.query.clientId;
     const secretKey = req.query.secretKey;
     if (!secretKey || secretKey !== process.env.SECRET_KEY) {
@@ -128,7 +139,7 @@ app.get('/status', (req, res, next) => {
     }
 });
 
-app.get('/message', async (req, res, next) => {
+app.get('/message', async (req, res) => {
     const requestedClientId = req.query.clientId;
     const number = req.query.to;
     const text = req.query.message;
@@ -137,25 +148,23 @@ app.get('/message', async (req, res, next) => {
     if (!secretKey || secretKey !== process.env.SECRET_KEY) {
         return res.status(401).json({ error: 'Unauthorized. Invalid secret key.' });
     }
-    
 
     if (!requestedClientId || !number || !text || !clientPhoneNumber) {
-        res.status(400).json({ error: 'clientId, number, text and clientPhoneNumber are required in the query parameters' });
+        res.status(400).json({ error: 'clientId, number, text, and clientPhoneNumber are required in the query parameters' });
         return;
     }
+
     const client = activeClients[requestedClientId];
     if (client) {
-        const user = client.info.me.user
+        const user = client.info.me.user;
         if (user !== clientPhoneNumber) {
             console.log(`User and client phone number do not match for ${requestedClientId}`);
             res.status(400).json({ error: 'User and client phone number do not match' });
             return;
-        
-    }
+        }
         const chatId = number.substring(1) + "@c.us";
-        client.sendMessage(chatId, text);
-
         try {
+            await client.sendMessage(chatId, text);
             await prisma.messageLog.create({
                 data: {
                     clientId: requestedClientId,
@@ -165,7 +174,7 @@ app.get('/message', async (req, res, next) => {
             });
             res.status(200).json({ message: `Client ${requestedClientId} sent message successfully` });
         } catch (error) {
-            console.error("Error saving message log:", error);
+            console.error("Error sending message or saving message log:", error);
             res.status(500).json({ error: 'Internal server error' });
         }
     } else {
@@ -173,34 +182,9 @@ app.get('/message', async (req, res, next) => {
     }
 });
 
-
-
-// app.get('/logout', (req, res, next) => {
-//     const requestedClientId = req.query.clientId;
-
-//     if (!requestedClientId) {
-//         res.status(400).json({ error: 'clientId is required in the query parameters' });
-//         return;
-//     }
-
-//     const client = activeClients[requestedClientId];
-//     console.log(client);
-    
-//     if (client) {
-//         client.on('logout', (reason) => {
-//             // client.destroy();
-//             console.log(`Client for ${requestedClientId} was logged out`, reason);
-//             delete activeClients[requestedClientId];
-//             client.initialize()
-
-//         });
-    
-//         res.status(200).json({ message: `Client ${requestedClientId} was logged out` });
-//     } else {
-//         res.status(404).json({ error: `Client ${requestedClientId} not found or not ready` });
-//     }
-// });
-
 app.listen(port, () => {
     console.log(`Server is running on http://207.244.239.151:${port}`);
 });
+
+                   
+
